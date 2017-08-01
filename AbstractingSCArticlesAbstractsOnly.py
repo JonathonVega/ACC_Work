6ca0eb2ce151cee9e1cb72f7ad7c839b3e073847
Jonathon Vega <jfvega@live.com>
2017-08-01 09:45:33 -0400

    Update AbstractingSCArticlesAbstractsOnly.py

--- AbstractingSCArticlesAbstractsOnly.py
+++ AbstractingSCArticlesAbstractsOnly.py
@@ -6,8 +6,6 @@ from bs4 import BeautifulSoup
 
 suppSites = ['http://www.onlinejacc.org/content/meeting-abstract-supplements', 'http://www.interventions.onlinejacc.org/content/meeting-abstract-supplements']
 
-print('Hello, World!')
-
 def getScienceDirectLinks():
     scienceDirectLinks = []
     for site in suppSites:
@@ -19,14 +17,40 @@ def getScienceDirectLinks():
             if('www.sciencedirect.com' in links.get('href')):
                 scienceDirectLinks.append(links.get('href'))
                 #print(links.get('href'))
-    print(scienceDirectLinks)
+    #print(scienceDirectLinks)
+    return scienceDirectLinks
+
+# example: http://www.sciencedirect.com/science/journal/07351097/69/11/supp/S
+SDLinksList = getScienceDirectLinks()
+print(SDLinksList)
+print("cool")
+wbOriginal = openpyxl.load_workbook('SCArticlesAbstractsOnly.xlsx')
+
+print(wbOriginal.get_sheet_names())
+sheetOriginal = wbOriginal.get_sheet_by_name('SCArticles')
+
+numOfRows = sheetOriginal.max_row
+numOfColumns = sheetOriginal.max_column
+print(numOfColumns)
+
+filepath = 'C:\\Users\\jvega\\Documents\\PythonScripts\\SCAAONew.xlsx'
+wbNew = openpyxl.Workbook()
+wbNew.save(filepath)
+print('We done?')
+
+#sheetNew = wbNew.get_sheet_by_name('Sheet')
+wbNew = openpyxl.load_workbook('SCAAONew.xlsx')
+sheetNew = wbNew.get_sheet_by_name('Sheet')
+
+for column in range(1, numOfColumns + 1):
+    sheetNew.cell(row=1, column=column).value = sheetOriginal.cell(row=1, column=column).value
+
+wbNew.save('SCAAONew.xlsx')
 
 
-getScienceDirectLinks()
-time.sleep(100)
 
-#wbOriginal = openpyxl.load_workbook('SCArticlesAbstractsOnly')
 
-#print(wbOriginal.get_sheet_names())
-#sheetOriginal = wbOriginial.get_sheet_by_name('SCArticles')
-#sheetOriginal[]
+
+
+
+time.sleep(10)
