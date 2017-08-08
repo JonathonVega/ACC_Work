46b7c60cdcaa2d8c688639a4018b0204c0a1e851
Jonathon Vega <jfvega@live.com>
2017-08-08 11:40:02 -0400

    Cleaned and commented code

--- HtmlToText.py
+++ HtmlToText.py
@@ -4,19 +4,9 @@ import requests
 import csv
 from requests.auth import HTTPBasicAuth
 import time
+import datetime
 
-JACCJournal = {"JACC": "http://www.onlinejacc.org"}
-INTJournal = {"INT":"http://www.interventions.onlinejacc.org"}
-BTSJournal = {"BTS": "http://www.basictranslational.onlinejacc.org"}
-IMGJournal = {"IMG": "http://www.imaging.onlinejacc.org"}
-EPJournal = {"EP": "http://www.electrophysiology.onlinejacc.org"}
-HFJournal = {"HF": "http://www.heartfailure.onlinejacc.org"}
-
-otherJournals = {"BTS": "http://www.basictranslational.onlinejacc.org",
-"IMG": "http://www.imaging.onlinejacc.org",
-"EP": "http://www.electrophysiology.onlinejacc.org",
-"HF": "http://www.heartfailure.onlinejacc.org"}
-
+# All the web links to the JACC Journals
 JACCJournals = {"JACC": "http://www.onlinejacc.org",
 "BTS": "http://www.basictranslational.onlinejacc.org",
 "IMG": "http://www.imaging.onlinejacc.org",
@@ -26,52 +16,56 @@ JACCJournals = {"JACC": "http://www.onlinejacc.org",
 
 skipArticlesList = ['/content/48/5/e247', '/content/50/7/e1', '/content/52/13/e1', '/content/48/4/e149', '/content/48/3/e1','/content/50/17/e159', '/content/61/23/e179', '/content/60/24/e44', '/content/58/24/e123'] #The links to these pages don't work
 
+# Uses Beautiful Soup to retrieve the archive source page from the journal's link
 def getArchive(journalLink):
     r = requests.get(journalLink + "/content/by/year")
     archiveSoup = BeautifulSoup(r.text, 'lxml')
     return archiveSoup
 
+# Returns all the issues of the selected year from the chosen Journal in a list
+# Example: /content/70/7
 def getIssues(year, JLink):
-        issues = []
+        issuesList = []
         site = JLink + "/content/by/year/" + year
         print(site)
         archiveYearSoup = BeautifulSoup((requests.get(site)).text, "lxml")
         for issue in archiveYearSoup.find_all("a", {"class": "hw-issue-meta-data"}):
-            issues.append(issue.get("href"))
-        return issues
+            issuesList.append(issue.get("href"))
+        return issuesList
 
+# There are some Articles in an issue's page that isn't from that certain issue such as the 'Journal Impact' section
+# This block will keep these unwanted articles from being downloaded
 def isArticleRelatedToIssue(issueLink, href):
     if issueLink in href:
         return True
     else:
         return False
 
+# Returns a list of articles from the selected issue from the chosen Journal
 def getArticles(issueLink, JLink):
     issueSoup = BeautifulSoup((requests.get(JLink + issueLink)).text, 'lxml')
-    articles = []
+    articlesList = []
     for article in issueSoup.find_all("a", {"class": "highwire-cite-linked-title"}):
         if isArticleRelatedToIssue(issueLink, article.get("href")):
-            articles.append(article.get("href"))
+            articlesList.append(article.get("href"))
             print(article.get("href"))
-    return articles
+    return articlesList
 
-def getDataTitlesList(metaData):
-    dataTitles = []
-    for k in metaData[0]:
-        dataTitles.append(k)
-    return dataTitles
-
-def skipArticle(article): # Should only be used when you want to skip to an issue
+# This block should only be used when you don't want the script to download certain articles in issues
+# Shouldn't be used at all unless you need to skip or get certain articles without having the script to read the all the issues in that year
+def skipArticle(article):
     if "content/4/2/" in article or "content/4/1/" in article:
-        return False
-    else:
         return True
+    else:
+        return False
 
+# This block is meant to get the source pages from articles that are restricted
+# Will need to input your JACC USERNAME and PASSWORD to access them
 def restrictedArticleSourceToTxt(articleLink, JLink):
     with requests.Session() as c:
         USERNAME = 'XXX'
         PASSWORD = 'XXX'
-        url = 'http://auth.acc.org/ACCFederatedLogin/Login?SP=HW&src=HW&target=http%3A%2F%2Fwww.onlinejacc.org%2Fcontent%2Fby%2Fyear%3Fsso%3D1%26sso_redirect_count%3D1&destination=/content/by/year&_ga=2.92849885.190577121.1499866663-938354422.1499434034'
+        url = 'http://auth.acc.org/ACCFederatedLogin/Login?SP=HW&src=HW&target=http%3A%2F%2Fwww.onlinejacc.org%2Fcontent%2Fby%2Fyear%3Fsso%3D1%26sso_redirect_count%3D1&destination=/content/by/year&_ga=2.92849885.190577121.1499866663-938354422.1499434034' # This is the link to the sign-in page
         c.get(url)
         login_data = dict(UserName=USERNAME, Password=PASSWORD, next='/')
         c.post(url, data=login_data, headers={'Referer': 'http://www.onlinejacc.org'})
@@ -85,6 +79,7 @@ def restrictedArticleSourceToTxt(articleLink, JLink):
             tf.write((page.content).decode('utf-8'))
             tf.close()
 
+# This block is meant to get the source pages from articles that are open-access
 def openArticleSourceToTxt(articleLink, JLink):
     fileName = articleLink + ".txt"
     webLink = JLink + articleLink
@@ -94,6 +89,8 @@ def openArticleSourceToTxt(articleLink, JLink):
     print(webLink)
     urllib.request.urlretrieve(webLink, fileName)
 
+# This block will ignore the article links inside 'skipArticlesList' since those articles don't work
+# Should not be used at all unless you come across recent articles that don't load when clicked, therefore you should include them in 'skipArticlesList' so the program can run for the rest
 def stoppedRunning(articleLink):
     stoppedRunning = False
     for i in skipArticlesList:
@@ -103,12 +100,12 @@ def stoppedRunning(articleLink):
 
 
 # Instructions for the user
-print('!!!Make sure this script is inside the folder that you want all the article\'s source code in. Otherwise you\'ll have hundreds of files somewhere that you don\'t want them in. I\'d recommend copying and pasting this program into the folder you want the article\'s source pages in.!!!\n')
+print('!!!Make sure this script is inside the folder that you want all the article\'s source code in. Otherwise you\'ll have hundreds of files somewhere that you don\'t want them in. I\'d recommend copying and pasting this program into the folder you want the article\'s source pages in.!!!\n!!!Also make sure that your JACC username and JACC password are in this script in the method \'restrictedArticleSourceToTxt\' so that the program is able to access the restricted content.!!!\n')
 print('JACC - Journal of American College of Cardiology\nBTS - Basic to Translational Science\nIMG - Cardiovascular Imaging\nINT - Cardiovascular Interventions\nEP - Clinical Electrophysiology\nHF - Heart Failure')
 journal = input('Please choose the journal you\'d like to update: ')
 journal = journal.upper()
 
-# This block was made to make sure the right journal is chosen
+# This block was made to make sure a valid journal is chosen by the user
 while(journal != 'JACC' or journal != 'BTS' or journal != 'IMG' or journal != 'INT' or journal != 'EP' or journal != 'HF'):
     journal = input('Please choose a valid journal: ')
     journal = journal.upper()
@@ -118,21 +115,20 @@ while(journal != 'JACC' or journal != 'BTS' or journal != 'IMG' or journal != 'I
         continue
 
 print(journal)
-soup = getArchive(JACCJournals[journal])
+archiveSoup = getArchive(JACCJournals[journal]) #Reads the journal's archive source page
 year = input('What year you would like to update\nExample:\"2017\"\nChoose your Year: ')
-issues = getIssues(year, JACCJournals[key])
-for issue in issues:
-    articles = getArticles(issue, JACCJournals[key])
-    for article in articles:
+issuesList = getIssues(year, JACCJournals[journal])
+for issue in issuesList:
+    articlesList = getArticles(issue, JACCJournals[journal])
+    for article in articlesList:
+        now = datetime.datetime.now()
+        print(now.year)
+        print(int(now.year))
         if skipArticle(article):
             continue
         if stoppedRunning(article):
             continue
-        elif (year == "2016" or year == "2017") and (key == "JACC" or key == "IMG" or key == "INT" or key == "EP" or key == "HF"):
-            restrictedArticleSourceToTxt(article, JACCJournals[key])
+        elif (year == str((int(now.year) - 1)) or year == str(now.year)) and (journal == "JACC" or journal == "IMG" or journal == "INT" or journal == "EP" or journal == "HF"):
+            restrictedArticleSourceToTxt(article, JACCJournals[journal])
         else:
-            openArticleSourceToTxt(article, JACCJournals[key])
-
-
-
-#createCSV(allMeta)
+            openArticleSourceToTxt(article, JACCJournals[journal])
